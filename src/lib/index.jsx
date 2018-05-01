00572f6dd661e39269cb3b65c2e9ba9881f5c539
js-jslog <jhs4jbs@hotmail.co.uk>
2018-05-01 08:02:42 +0000

    adding notes to the index.jsx

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 
-class MySuperCoolComponent extends Component {
-  handleClick = () => {
-    console.log("Click!");
-  };
-
+// provide another component for the scoreboard & control panel rendering and api call to the start game function
+class WingTron extends Component {
+  sizeCanvas = () => {
+    // set the canvas to fill the component it is placed inside
+    // this should probably be done using css for the first 2 lines
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+  }
+  constructor(props) {
+    this.sizeCanvas();
+    // set a load of presets incase no control panel is provided
+    // also render a "click here to start" text on the canvas if there is no control panel
+    // a lot of what will be done here is contained in the startWingTronGame function
+  }
   render() {
     const { color, children } = this.props;
     return (
-      <button onClick={this.handleClick} style={{ color }}>
-        {children}
-      </button>
+      <canvas>
+      </canvas>
     );
   }
 }
 
-export default MySuperCoolComponent;
+export default WingTron;
