5748ac8cdeb9a78e8eb776e99bb87018565f22f8
js-jslog <jhs4jbs@hotmail.co.uk>
2018-05-07 14:08:32 +0100

    moving some of the options in to state in prep for fluxifying

--- src/lib/WingTron.jsx
+++ src/lib/WingTron.jsx
@@ -5,6 +5,20 @@ import ApplicationInterface from "./applicationInterface.js";
 class WingTron extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      gameOptions: {
+        fieldWidth: "300", fieldHeight: "300", matches: "10",
+      },
+      playerOptions: [
+        {"startCoord": [150,150], "direction": 0, "keyCodes": {"leftCode": 37, "rightCode": 39}, "colour": "rgba(255,0,0,0.5)",},
+        {"startCoord": [150,150], "direction": Math.PI, "keyCodes": {"leftCode": 65, "rightCode": 68}, "colour": "rgba(0,0,255,0.5)",},
+      ],
+      environmentOptions: {
+        scoreboardFunction: function (a) {
+          console.log(a);
+        }
+      },
+    };
     // set a load of presets incase no control panel is provided
     // also render a "click here to start" text on the canvas if there is no control panel
     // a lot of what will be done here is contained in the startWingTronGame function
@@ -15,17 +29,9 @@ class WingTron extends Component {
   updateCanvas() {
     var canvas = this.refs.canvas;
     var environmentOptions = {};
-    var gameOptions = {fieldWidth: "300", fieldHeight: "300", matches: "10"};
-    var p1Options = {"startCoord": [150,150], "direction": 0, "keyCodes": {"leftCode": 37, "rightCode": 39}, "colour": "rgba(255,0,0,0.5)"};
-    var p2Options = {"startCoord": [150,150], "direction": Math.PI, "keyCodes": {"leftCode": 65, "rightCode": 68}, "colour": "rgba(0,0,255,0.5)"};
-    var playerOptions = [p1Options, p2Options];
-    var options = {"environmentOptions": environmentOptions, "gameOptions": gameOptions, "playerOptions": playerOptions};
+    var options = {"environmentOptions": this.state.environmentOptions, "gameOptions": this.state.gameOptions, "playerOptions": this.state.playerOptions};
 
     options.environmentOptions.canvas = canvas;
-    options.environmentOptions.scoreboardFunction = function (a) {
-      console.log(a);
-    }
-
     canvas.width = options.gameOptions.fieldWidth;
     canvas.height = options.gameOptions.fieldHeight;
     canvas.style.width = '100%';
