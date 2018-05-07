21fad7360cf7c5c45a30f70d5bfdfea3cf406670
js-jslog <jhs4jbs@hotmail.co.uk>
2018-05-07 14:56:48 +0100

    created an OptionStore for options management in flux

---
+++ src/lib/optionsStore.js
@@ -0,0 +1,29 @@
+import { EventEmitter } from 'events';
+
+class OptionsStore extends EventEmitter {
+  constructor() {
+    super();
+    this.options = {
+      gameOptions: {
+        fieldWidth: "300", fieldHeight: "300", matches: "10",
+      },
+      playerOptions: [
+        {"startCoord": [150,150], "direction": 0, "keyCodes": {"leftCode": 37, "rightCode": 39}, "colour": "rgba(255,0,0,0.5)",},
+        {"startCoord": [150,150], "direction": Math.PI, "keyCodes": {"leftCode": 65, "rightCode": 68}, "colour": "rgba(0,0,255,0.5)",},
+      ],
+      environmentOptions: {
+        scoreboardFunction: function (a) {
+          console.log(a);
+        },
+      },
+    };
+  }
+
+  getAll() {
+    return this.options;
+  }
+}
+
+const optionsStore = new OptionsStore;
+
+export default optionsStore;
