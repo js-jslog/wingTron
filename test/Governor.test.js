38903536ddc8732b9f294f664c68e122379016e9
js-jslog <jhs4jbs@hotmail.co.uk>
2018-05-13 00:24:31 +0100

    little readability change

--- test/Governor.test.js
+++ test/Governor.test.js
@@ -1,6 +1,20 @@
 import Governor from '../src/restructure/Governor/Governor.js'
 import OptionsStore from '../src/restructure/OptionsStore.js'
 
+const valid_options = {
+  field_width: '200',
+  field_height: '200',
+  matches: '10',
+  player_options: [{
+    start_coord_x: '200',
+    start_coord_y: '200',
+    direction: '0',
+    turn_left_keycode: '65',
+    turn_right_keycode: '68',
+    colour: 'rgba(0,0,255, 0.5)',
+  }]
+}
+
 test('that the governor fails to start the game if there are no options', () => {
   OptionsStore.setOptions({});
   const governor = new Governor()
@@ -8,19 +22,7 @@ test('that the governor fails to start the game if there are no options', () => 
 })
 
 test('that governor can start game if the options are available', () => {
-  OptionsStore.setOptions({
-    field_width: '200',
-    field_height: '200',
-    matches: '10',
-    player_options: [{
-      start_coord_x: '200',
-      start_coord_y: '200',
-      direction: '0',
-      turn_left_keycode: '65',
-      turn_right_keycode: '68',
-      colour: "rgba(0,0,255, 0.5)",
-    }]
-  })
+  OptionsStore.setOptions(valid_options)
   const governor = new Governor()
   return expect(governor.startGame()).toBeTruthy()
 })
