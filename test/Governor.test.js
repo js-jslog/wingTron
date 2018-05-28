296b7cc20ab936f12e1f29a6fb0732c9e7de4dad
js-jslog <jhs4jbs@hotmail.co.uk>
2018-05-28 19:51:58 +0100

    improvements to the governor tests (making use of describe)

--- test/Governor.test.js
+++ test/Governor.test.js
@@ -37,17 +37,20 @@ beforeEach(() => {
   GameStore.state = undefined
 })
 
-test('that the governor fails to start the game if there are no options', () => {
-  return expect(Governor.startGame()).toBeFalsy()
-})
+describe('the governors game setup', () => {
 
-test('that governor can start game if the options are available', () => {
-  OptionsStore.options = valid_options
-  return expect(Governor.startGame()).toBeTruthy()
-})
+    test('that the governor fails to start the game if there are no options', () => {
+        return expect(Governor.startGame()).toBeFalsy()
+    })
 
-test('that governor creates a game store state', () => {
-  OptionsStore.options = valid_options
-  Governor.startGame()
-  expect(GameStore.state).toEqual(valid_converted_game_state)
+    test('that governor can start game if the options are available', () => {
+        OptionsStore.options = valid_options
+        return expect(Governor.startGame()).toBeTruthy()
+    })
+
+    test('that governor creates a game store state', () => {
+        OptionsStore.options = valid_options
+        Governor.startGame()
+        expect(GameStore.state).toEqual(valid_converted_game_state)
+    })
 })
