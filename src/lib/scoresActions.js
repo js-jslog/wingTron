f0a9a67a4f54423398f45ac9174945c7f1ce887d
js-jslog <jhs4jbs@hotmail.co.uk>
2018-07-15 11:28:28 +0100

    just removing some semi-colons from a file

--- src/lib/scoresActions.js
+++ src/lib/scoresActions.js
@@ -1,8 +1,8 @@
-import dispatcher from './dispatcher';
+import dispatcher from './dispatcher'
 
 export function updateScores(scores) {
   dispatcher.dispatch({
     type: 'UPDATE_SCORES',
     scores: scores,
-  });
+  })
 }
