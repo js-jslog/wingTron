69953c395acf4475e64385d2f514d9a1416a4aad
js-jslog <jhs4jbs@hotmail.co.uk>
2018-07-15 14:57:27 +0100

    removing the governors reset function. this simply isn't needed

--- src/restructure/Governor/Governor.js
+++ src/restructure/Governor/Governor.js
@@ -4,13 +4,6 @@ import GameStore from '../GameStore.js'
 
 class Governor {
 
-  reset() {
-    GameStore.state = {
-      status: GameStore.NOT_STARTED && GameStore.NOT_STARTED,
-    }
-    OptionsStore.options = OptionsStore.DEFAULT_OPTIONS
-  }
-
   startGame() {
     startNewGame()
   }
--- test/Governor.test.js
+++ test/Governor.test.js
@@ -4,28 +4,11 @@ import OptionsStore from '../src/restructure/OptionsStore.js'
 import * as dependency from '../src/restructure/GameActions'
 import CanvasDrawer from '../src/restructure/CanvasDrawer.js'
 
-describe('the governors resetting functionality', () => {
-
-  test('that the GameStore is set back to NOT_STARTED', () => {
-    GameStore.state = undefined
-    Governor.reset()
-
-    expect(GameStore.NOT_STARTED).toBe('NOT_STARTED')
-    expect(GameStore.state).toEqual({status: GameStore.NOT_STARTED})
-  })
-
-  test('that the OptionsStore is set back to the default values', () => {
-    OptionsStore.options = undefined
-    Governor.reset()
-
-    expect(OptionsStore.options).toEqual(OptionsStore.DEFAULT_OPTIONS)
-  })
-})
-
 describe('the governors game setup', () => {
 
   beforeEach(() => {
-    Governor.reset()
+    OptionsStore.options = OptionsStore.DEFAULT_OPTIONS
+    GameStore.state = GameStore.DEFAULT_STATE
     dependency.startNewGame = jest.fn()
   })
 
