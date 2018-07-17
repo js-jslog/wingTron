6d980ee737079ae1a0563b8a7d8a273d98a8a380
js-jslog <jhs4jbs@hotmail.co.uk>
2018-07-17 02:00:43 +0100

    removing redundant functionality

--- src/restructure/GameActions/reducePlayerPaths.js
+++ src/restructure/GameActions/reducePlayerPaths.js
@@ -1,14 +1,3 @@
-export default function reducePlayerPaths(game_state) {
-  const reduced_state = game_state.player_state.map(player_state => {
-    const reduced_obj = {
-      path: player_state.path,
-      direction: player_state.direction,
-    }
-    return reduced_obj
-  })
-  return reduced_state
-}
-
 export function reducePlayerStates(player_state, state_item_name) {
   return player_state.map(player_state => player_state[state_item_name])
 }
--- test/GameActions/reducePlayerPaths.test.js
+++ test/GameActions/reducePlayerPaths.test.js
@@ -4,31 +4,6 @@ import optionsToGameState from '../../src/restructure/GameActions/optionsToGameS
 import GameStore from '../../src/restructure/GameStore.js'
 import OptionsStore from '../../src/restructure/OptionsStore.js'
 
-describe('the functionality to reduce a paths object from a complete GameStore state object', () => {
-
-  test('that a reduced copy of the input state object is returned', () => {
-    const expected_output = [
-      {
-        path: [
-          [150, 100 ],
-          [150, 100 ],
-        ],
-        direction: 0,
-      },
-      {
-        path: [
-          [150, 100 ],
-          [150, 100 ],
-        ],
-        direction: Math.PI,
-      },
-    ]
-    const game_state = optionsToGameState(OptionsStore.DEFAULT_OPTIONS)
-
-    expect(reducePlayerPaths(game_state)).toEqual(expected_output)
-  })
-})
-
 describe('the player state reducer', () => {
 
   test('that the reducer works on paths', () => {
