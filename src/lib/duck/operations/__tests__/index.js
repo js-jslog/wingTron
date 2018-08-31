5c96b98a264826bc8170de8dd62b63321864ba4a
js-jslog <jhs4jbs@hotmail.co.uk>
2018-09-01 00:09:13 +0100

    adding some simple operations

---
+++ src/lib/duck/operations/__tests__/index.js
@@ -0,0 +1,20 @@
+import * as ActionCreators from '~/duck/actions/'
+import * as ActionOperations from '../'
+
+describe('the pre-dispatch action operations', () => {
+
+  test('that the updateOptions operation is identical to the updateOptions action creator', () => {
+
+    expect(ActionOperations.updateOptions).toEqual(ActionCreators.updateOptions)
+  })
+
+  test('that the addPlayer operation is identical to the addPlayer action creator', () => {
+
+    expect(ActionOperations.addPlayer).toEqual(ActionCreators.addPlayer)
+  })
+
+  test('that the removePlayer operation is identical to the removePlayer action creator', () => {
+
+    expect(ActionOperations.removePlayer).toEqual(ActionCreators.removePlayer)
+  })
+})
---
+++ src/lib/duck/operations/index.js
@@ -0,0 +1,7 @@
+import * as ActionCreators from '~/duck/actions/'
+
+export const updateOptions = ActionCreators.updateOptions
+
+export const addPlayer = ActionCreators.addPlayer
+
+export const removePlayer = ActionCreators.removePlayer
