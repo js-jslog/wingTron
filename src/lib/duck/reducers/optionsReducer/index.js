c1502b041b4944332c4bf54900cdeeaf3a425ed6
js-jslog <jhs4jbs@hotmail.co.uk>
2018-09-09 01:04:57 +0100

    forgot to remove the functionality which has been moved to the playerOptionsReducer

--- src/lib/duck/reducers/optionsReducer/index.js
+++ src/lib/duck/reducers/optionsReducer/index.js
@@ -32,27 +32,6 @@ export const optionsReducer = (state_in=INITIAL_STATE, action) => {
       return { ...action.options }
     }
 
-    case ActionTypes.ADD_PLAYER_TO_OPTIONS: {
-      const { players } = state_in
-
-      return {
-        ...state_in,
-        players: [ ...players, {} ]
-      }
-    }
-
-    case ActionTypes.REMOVE_PLAYER_FROM_OPTIONS: {
-      const { index } = action
-      const players = [ ...state_in.players ]
-
-      players.splice(index, 1)
-
-      return {
-        ...state_in,
-        players
-      }
-    }
-
     default: return state_in
   }
 }
