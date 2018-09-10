7797250d5b35612ced33785ec1ef330258708f94
js-jslog <jhs4jbs@hotmail.co.uk>
2018-09-10 12:01:57 +0000

    adding first flow type

--- src/lib/duck/reducers/gameReducer/players/playerOptionsToState/optionConverter/index.js
+++ src/lib/duck/reducers/gameReducer/players/playerOptionsToState/optionConverter/index.js
@@ -1,4 +1,15 @@
-export const optionsConverter = (options) => {
+// @flow
+
+type PlayerOptions = {
+  start_coord_x: string,
+  start_coord_y: string,
+  direction: string,
+  turn_left_keycode: string,
+  turn_right_keycode: string,
+  colour: string
+}
+
+export const optionsConverter = (options: PlayerOptions) => {
   const state = { ...options }
   state.start_coord_x = parseInt(state.start_coord_x)
   state.start_coord_y = parseInt(state.start_coord_y)
