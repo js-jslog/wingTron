4358bd68729d3b600eb383b723a6d4b7c18b47d6
js-jslog <jhs4jbs@hotmail.co.uk>
2018-09-18 20:52:46 +0100

    tidying the root level flow types

--- src/lib/common/flow-types/index.js
+++ src/lib/common/flow-types/index.js
@@ -1,21 +1,18 @@
 // @flow
 
 import type { Options as OptionsImport } from '~/duck/reducers/optionsReducer'
-
 import type { Game as GameImport } from '~/duck/reducers/gameReducer'
-import type { Match as MatchImport } from '~/duck/reducers/gameReducer/match/types'
-import type { Players as PlayersImport } from '~/duck/reducers/gameReducer/players/types'
-import type { Paths } from '~/duck/reducers/gameReducer/paths/types'
+export type Options = OptionsImport
+export type Game = GameImport
 
 export type { MatchOptions } from '~/duck/reducers/optionsReducer/matchOptionsReducer/types'
 export type { PlayerOptions } from '~/duck/reducers/optionsReducer/playerOptionsReducer/types'
 export type { PlayerOptionsSet } from '~/duck/reducers/optionsReducer/playerOptionsReducer/types'
-export type Options = OptionsImport
 
-export type Game = GameImport
-export type Match = MatchImport
+export type { Match } from '~/duck/reducers/gameReducer/match/types'
+export type { Paths } from '~/duck/reducers/gameReducer/paths/types'
 export type { Player } from '~/duck/reducers/gameReducer/players/types'
-export type Players = PlayersImport
+export type { Players } from '~/duck/reducers/gameReducer/players/types'
 
 export type { Action } from '~/duck/actions/types'
 
