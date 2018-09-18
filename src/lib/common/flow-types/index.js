e609f6f20996ffe9640c0b518bf933d22f25ebb0
js-jslog <jhs4jbs@hotmail.co.uk>
2018-09-18 20:25:26 +0100

    removing types which don't need to exist at root level

--- src/lib/common/flow-types/index.js
+++ src/lib/common/flow-types/index.js
@@ -4,17 +4,11 @@ import type { Options as OptionsImport } from '~/duck/reducers/optionsReducer'
 
 import type { GameMatch as GameMatchImport } from '~/duck/reducers/gameReducer/match/types'
 
-import type { Player as PlayerImport } from '~/duck/reducers/gameReducer/players/types'
-import type { PlayersById as PlayersByIdImport } from '~/duck/reducers/gameReducer/players/types'
-import type { PlayerList as PlayerListImport } from '~/duck/reducers/gameReducer/players/types'
+export type { Player } from '~/duck/reducers/gameReducer/players/types'
 import type { Players as PlayersImport } from '~/duck/reducers/gameReducer/players/types'
 
 import type { Paths } from '~/duck/reducers/gameReducer/paths/types'
 
-export type { UpdateOptionsAction } from '~/duck/actions/types'
-export type { AddPlayerToOptionsAction } from '~/duck/actions/types'
-export type { RemovePlayerFromOptionsAction } from '~/duck/actions/types'
-export type { StartGameFromOptionsAction } from '~/duck/actions/types'
 export type { Action } from '~/duck/actions/types'
 
 export type { PlayerOptions } from '~/duck/reducers/optionsReducer/playerOptionsReducer/types'
@@ -24,9 +18,6 @@ export type Options = OptionsImport
 
 export type GameMatch = GameMatchImport
 
-export type Player = PlayerImport
-export type PlayersById = PlayersByIdImport
-export type PlayerList = PlayerListImport
 export type Players = PlayersImport
 
 export type State = {|
