7321e4c5eb10f19fc08abd3edf66c2ed8911cf88
js-jslog <jhs4jbs@hotmail.co.uk>
2018-10-14 17:52:05 +0100

    adding a handler to translate the keypress in to the it's keycode in the turn input fields

--- src/docs/App/ControlPanelRedux/PlayerOptionsSet/PlayerOptionsUI/index.js
+++ src/docs/App/ControlPanelRedux/PlayerOptionsSet/PlayerOptionsUI/index.js
@@ -23,23 +23,45 @@ export class PlayerOptionsUI extends Component<Props, null> {
         <h3>Player{ index }</h3>
         <div>
           <span>Start coords: (x)</span>
-          <input className='start_coord_x' value={ player.start_coord_x } onChange={ this.updatePlayerOption.bind(this) } />
+          <input
+            className='start_coord_x'
+            value={ player.start_coord_x }
+            onChange={ this.updatePlayerOption.bind(this) } />
           <span> (&amp; y)</span>
-          <input className='start_coord_y' value={ player.start_coord_y } onChange={this.updatePlayerOption.bind(this)} />
+          <input
+            className='start_coord_y'
+            value={ player.start_coord_y }
+            onChange={this.updatePlayerOption.bind(this)} />
         </div>
         <div>
           <span>Direction</span>
-          <input className='direction' value={ player.direction } onChange={this.updatePlayerOption.bind(this)} />
+          <input
+            className='direction'
+            value={ player.direction }
+            onChange={this.updatePlayerOption.bind(this)} />
         </div>
         <div>
           <span>Left key</span>
-          <input className='turn_left_keycode' value={ player.turn_left_keycode } onChange={this.updatePlayerOption.bind(this)} onKeyDown={ this.updatePlayerOption.bind(this) } />
+          <input
+            className='turn_left_keycode'
+            value={ player.turn_left_keycode }
+            onChange={this.updatePlayerOption.bind(this)}
+            onKeyDown={ translateKeyCode }
+          />
           <span>Right key</span>
-          <input className='turn_right_keycode' value={ player.turn_right_keycode } onChange={this.updatePlayerOption.bind(this)} onKeyDown={ this.updatePlayerOption.bind(this) } />
+          <input
+            className='turn_right_keycode'
+            value={ player.turn_right_keycode }
+            onChange={ this.updatePlayerOption.bind(this) }
+            onKeyDown={ translateKeyCode }
+          />
         </div>
         <div>
           <span>Colour</span>
-          <input className='colour' value={ player.colour } onChange={this.updatePlayerOption.bind(this)} />
+          <input
+            className='colour'
+            value={ player.colour }
+            onChange={this.updatePlayerOption.bind(this)} />
         </div>
         <div>
           <button onClick={this.updatePlayerOption.bind(this)}>Remove this player</button>
@@ -54,5 +76,11 @@ export class PlayerOptionsUI extends Component<Props, null> {
     const { value } = event.target
     this.props.updatePlayerOption(index, className, value)
   }
+}
+
+const translateKeyCode = (event: SyntheticEvent) => {
+  event.preventDefault()
+  event.target.value = event.keyCode
+  return false;
 }
 
